bb4877666489e9cf253f78e928ebf8331c07368e
Curtis Alexander <calex@calex.org>
2018-02-24 15:37:28 -0600

    fix for travis

--- publish.sh
+++ publish.sh
@@ -1,14 +1,26 @@
 #!/usr/bin/env bash
 
-# setup git
+###########
+## Setup ##
+###########
 git config --global user.name "Curtis Alexander"
 git config --global user.email "calex@calex.org"
 
-# checkout
+#############################
+## Fetch, Checkout, Commit ##
+#############################
+
+# https://stackoverflow.com/a/44036486
+
+# Fetch all the remote branches. Travis clones with `--depth`, which
+# implies `--single-branch`, so we need to overwrite remote.origin.fetch to
+# do that.
+git config --replace-all remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+git fetch
+
 git checkout -b publish
 git add .
 git commit --message "Travis build: ${TRAVIS_BUILD_NUMBER}"
 
 git remote add origin-publish https://${GITHUB_TOKEN}@github.com/curtisalexander/calex.git > /dev/null 2>&1
 git push --quiet --set-upstream origin-publish publish
-
