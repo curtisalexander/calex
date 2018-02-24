ac49c9beaf6d3cbb12c270129bb3ce0cc5c58368
Curtis Alexander <calex@calex.org>
2018-02-24 15:45:49 -0600

    force as don't need local to mirror remote

--- publish.sh
+++ publish.sh
@@ -23,4 +23,4 @@ git add .
 git commit --message "Travis build: ${TRAVIS_BUILD_NUMBER}"
 
 git remote add origin-publish https://${GITHUB_TOKEN}@github.com/curtisalexander/calex.git > /dev/null 2>&1
-git push --quiet --set-upstream origin-publish publish
+git push -f --quiet --set-upstream origin-publish publish
