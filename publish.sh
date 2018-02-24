81633a31ec251b14f6c5f7faec596552fa989883
Curtis Alexander <calex@calex.org>
2018-02-24 15:58:00 -0600

    spacing

--- publish.sh
+++ publish.sh
@@ -3,8 +3,10 @@
 ###########
 ## Setup ##
 ###########
+
 git config --global user.name "Curtis Alexander"
 git config --global user.email "calex@calex.org"
+
 
 #############################
 ## Fetch, Checkout, Commit ##
@@ -22,5 +24,11 @@ git checkout -b publish
 git add .
 git commit --message "Travis build: ${TRAVIS_BUILD_NUMBER}"
 
+
+##########
+## Push ##
+##########
+
 git remote add origin-publish https://${GITHUB_TOKEN}@github.com/curtisalexander/calex.git > /dev/null 2>&1
+# force as don't care to keep local in sync with remote
 git push -f --quiet --set-upstream origin-publish publish
